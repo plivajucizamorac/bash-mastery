6b362f9fcab3996f926edd0bdd67f60b28c593dc
root <root@DTDL136.devtech.local>
2023-05-29 22:54:01 +0200

    Finished section 01

---
+++ scripts/script01a.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Author: Mladen Djuric
+# Date created: 29/05/2023
+# Last modified: 29/05/2023
+
+# Description
+# Prints "This is my first script" to the terminal
+
+# Usage:
+# script01a
+
+echo "This is my first script"
+exit 0
